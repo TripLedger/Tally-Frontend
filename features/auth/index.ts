cfcbe3d9bc38a3483a2967ae84fdaad8e100cfc7
Yusrah Mohammed <mohammedyusi6@gmail.com>
2026-07-14 02:40:21 +0100

    fix: export displayNameSchema from auth so profile form builds

--- features/auth/index.ts
+++ features/auth/index.ts
@@ -1,6 +1,14 @@
 export { CurrencyPickerSheet } from "./CurrencyPickerSheet";
-export { emailAuthSchema, onboardingSchema } from "./schemas";
-export type { EmailAuthFormData, OnboardingFormData } from "./schemas";
+export {
+  emailAuthSchema,
+  onboardingSchema,
+  displayNameSchema,
+} from "./schemas";
+export type {
+  EmailAuthFormData,
+  OnboardingFormData,
+  DisplayNameFormData,
+} from "./schemas";
 export { useAuthSession, AuthSessionHydrator } from "./useAuthSession";
 export { EmailAuthSheet } from "./EmailAuthSheet";
 export { HeroMotion } from "./HeroMotion";
